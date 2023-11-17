af5d332e5b5f73c6674754ffe213340270da5592
Paulo Vitor Schio <paulovschio@gmail.com>
2023-11-16 21:35:35 -0300

    Adicionada tela de criar time

---
+++ lib/src/teams/create_team.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CreateTeam extends StatefulWidget {
+  const CreateTeam({super.key});
+
+  @override
+  State<CreateTeam> createState() => _CreateTeamState();
+}
+
+class _CreateTeamState extends State<CreateTeam> {
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar Time'),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: _pickImage,
+              child: CircleAvatar(
+                radius: 80,
+                backgroundImage:
+                    _imageFile != null ? FileImage(_imageFile!) : null,
+                child: _imageFile == null
+                    ? const Icon(Icons.add_a_photo, size: 40)
+                    : null,
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Nome do Time',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Descricão do Time',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ButtonTheme(
+              // Set the minimum width of the button to match the screen width
+              height: 50.0, // Set the height of the button
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.add),
+                label: const Text('Criar Time'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  minimumSize: const Size.fromHeight(50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(0), // Set the button shape
+                  ),
+                ),
+                onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text(
+                      'Time Criado!',
+                      textAlign: TextAlign.center,
+                    ),
+                    content: const Text('Clique para copiar o link de convite'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'OK'),
+                        child: const Text('Copiar'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
